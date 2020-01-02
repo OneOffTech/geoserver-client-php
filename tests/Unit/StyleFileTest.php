3ed592363b7cc59a09c00875fb6f4466dff4f482
Alessio <avvertix@users.noreply.github.com>
2020-01-02 17:16:54 +0100

    Test for PHP 7.2 and 7.3, fix style format identification (#14)

---
+++ tests/Unit/StyleFileTest.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ *    GeoServer PHP Client
+ *
+ *    Copyright (c) 2018 Oneoff-tech UG, Germany, www.oneofftech.xyz
+ *
+ *    This program is Free Software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public
+ *    License along with this program.  If not, see
+ *    <http://www.gnu.org/licenses/>.
+ */
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use OneOffTech\GeoServer\GeoFile;
+use OneOffTech\GeoServer\GeoType;
+use OneOffTech\GeoServer\GeoFormat;
+use OneOffTech\GeoServer\StyleFile;
+
+class StyleFileTest extends TestCase
+{
+    public function test_style_is_supported()
+    {
+        $supported = StyleFile::isSupported(__DIR__ . '/../fixtures/style.sld');
+        $this->assertTrue($supported);
+    }
+    
+    public function test_style_file_load()
+    {
+        $styleName = 'style_test';
+        $file = StyleFile::from(__DIR__ . '/../fixtures/style.sld')->name($styleName);
+
+        $this->assertInstanceOf(StyleFile::class, $file);
+        $this->assertEquals('application/vnd.ogc.sld+xml', $file->mimeType);
+        $this->assertEquals('sld', $file->extension);
+        $this->assertEquals('style_test', $file->name);
+        $this->assertEquals('style.sld', $file->originalName);
+    }
+}
